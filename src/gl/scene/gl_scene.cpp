b38a14ba1257d462e39a4e30b7d17fa892a90772
Christoph Oelckers <coelckers@zdoom.fake>
2014-11-16 09:08:47 +0100

    - now that we don't have to care anymore about SVN-related merging problems, let's save the 3 global variables of the GL renderer unconditionally, even if the software renderer is running.

--- src/gl/scene/gl_scene.cpp
+++ src/gl/scene/gl_scene.cpp
@@ -1102,6 +1102,10 @@ void FGLInterface::StateChanged(AActor *actor)
 void FGLInterface::StartSerialize(FArchive &arc)
 {
 	gl_DeleteAllAttachedLights();
+}
+
+void gl_SerializeGlobals(FArchive &arc)
+{
 	arc << fogdensity << outsidefogdensity << skyfog;
 }
 
